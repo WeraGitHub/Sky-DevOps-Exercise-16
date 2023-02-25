2f59375fdf27f263818d0a8573ae767ccb24c2b1
Weronika <weronikalimberger@gmail.com>
2023-02-25 11:49:32 +0000

    Re write code to reflect use of @property decorators

--- employee.py
+++ employee.py
@@ -10,68 +10,79 @@ class Employee(Person):
                  position, employee_id, start_date_day, start_day_month, start_day_year, salary, holidays=25):
         super().__init__(firstname, lastname, dob_day, dob_month, dob_year, gender, address)
         self.__company = company
-        self.department = department
-        self.position = position
+        self._department = department
+        self._position = position
         self.__employee_id = employee_id
         self.__employee = True
         self.__start_date = datetime.datetime(start_day_year, start_day_month, start_date_day)
-        self.salary = salary
-        self.holidays = holidays
+        self._salary = salary
+        self._holidays = holidays
         Employee.number_of_employees += 1
 
     def __str__(self):
-        return f"{self.get_firstname()} {self.get_lastname()} is an instance of a class Employee (Person)."
+        return f"{self.firstname} {self.lastname} is an instance of a class Employee (Person)."
 
-    def get_company(self):
+    @property
+    def company(self):
         return self.__company
 
-    def get_department(self):
-        return self.department
+    @property
+    def department(self):
+        return self._department
 
-    def set_department(self, new_department):
-        self.department = new_department
+    @department.setter
+    def department(self, new_department):
+        self._department = new_department
 
-    def get_position(self):
-        return self.position
+    @property
+    def position(self):
+        return self._position
 
-    def set_position(self, new_position):
-        self.position = new_position
+    @position.setter
+    def position(self, new_position):
+        self._position = new_position
 
-    def get_employee_id(self):
+    @property
+    def employee_id(self):
         return self.__employee_id
 
     def is_employee(self):
         return self.__employee
 
-    def get_start_date(self):
+    @property
+    def start_date(self):
         return self.__start_date
 
-    def get_salary(self):
-        return self.salary
+    @property
+    def salary(self):
+        return self._salary
 
-    def set_salary(self, new_salary):
-        self.salary = new_salary
+    @salary.setter
+    def salary(self, new_salary):
+        self._salary = new_salary
 
-    def get_holidays(self):
-        return self.holidays
+    @property
+    def holidays(self):
+        return self._holidays
 
-    def set_holidays(self, new_holidays):
-        self.holidays = new_holidays
+    @holidays.setter
+    def holidays(self, new_holidays):
+        self._holidays = new_holidays
 
     def work(self):
-        print(f"Employee {self.get_firstname()} is working.")
+        print(f"Employee {self.firstname} is working.")
 
     def lunch_break(self):
-        print(f"Employee {self.get_firstname()} is taking a lunch break")
+        print(f"Employee {self.firstname} is taking a lunch break")
 
     def take_holidays(self, number_of_days_taken):
-        holidays_left = self.get_holidays() - number_of_days_taken
-        self.set_holidays(holidays_left)
-        print(f"Employee {self.get_firstname()} is taking {number_of_days_taken} days of annual leave.")
+        holidays_left = self.holidays - number_of_days_taken
+        self._holidays = holidays_left
+        print(f"Employee {self.firstname} is taking {number_of_days_taken} days of annual leave.")
 
     def calculate_how_many_years_with_company(self):
         today = datetime.date.today()
-        start_date = self.get_start_date().date()
+        start_date = self.start_date
         years = today.year - start_date.year - ((today.month, today.day) < (start_date.month, start_date.day))
         return years
 
--- script_scenario.py
+++ script_scenario.py
@@ -2,7 +2,7 @@ from person import Person
 from customer import Customer
 from employee import Employee
 
-weronika = Person('Weronika', 'L', 10, 1, 1991, 'f', 'address 111')
+weronika = Person('Weronika', 'L', 10, 1, 1991, 'f', 'address111')
 
 print(str(weronika))
 weronika.walk()
@@ -14,16 +14,16 @@ print('Number of employee instances', Employee.get_number_of_employees())
 print('\n')
 
 weronika_customer = Customer('Weronika', 'L', 10, 1, 1991, 'f', 'address', 2)
-weronika_customer_2 = Customer('Weronika', 'L', 10, 1, 1991, 'f', 'address', 2)
+weronika_customer_2 = Customer('Weronika2', 'L2', 10, 10, 1991, 'f', 'address', 3)
 
 weronika_customer.walk()
 print(str(weronika_customer))
-print(f"{weronika_customer.get_firstname()} is a customer: {weronika_customer.is_customer()}, is also a person: "
+print(f"{weronika_customer.firstname} is a customer: {weronika_customer.is_customer()}, is also a person: "
       f"{weronika_customer.is_person()}")
 print(weronika_customer.get_customer_info())
 weronika_customer.purchase("lamp")
 weronika_customer.add_loyalty_points(500)
-print('Loyalty points:', weronika_customer.get_loyalty_points())
+print('Loyalty points:', weronika_customer.loyalty_points)
 print('Number of people instances', Person.get_number_of_person_instances())
 print('Number of customer instances', Customer.get_number_of_customers())
 print('Number of employee instances', Employee.get_number_of_employees())
@@ -38,12 +38,10 @@ weronika_employee.walk_to("work")
 weronika_employee.work()
 weronika_employee.lunch_break()
 weronika_employee.work()
-print('Holiday days available: ', weronika_employee.get_holidays())
+print('Holiday days available: ', weronika_employee.holidays)
 weronika_employee.take_holidays(5)
-print('Holiday days available: ', weronika_employee.get_holidays())
+print('Holiday days available: ', weronika_employee.holidays)
 print('Years with company:', weronika_employee.calculate_how_many_years_with_company())
-
-
 print('Number of people instances', Person.get_number_of_person_instances())
 print('Number of customer instances', Customer.get_number_of_customers())
 print('Number of employee instances', Employee.get_number_of_employees())
